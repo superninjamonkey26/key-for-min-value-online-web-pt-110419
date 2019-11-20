b37cc7f3312ab7ff8e8fd42f9fabf031051b290c
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-20 19:22:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   min_key = nil
   lowest_value = nil
   name_hash.each do |a, b|
-   # binding.pry
+    binding.pry
     if lowest_value == nil || b < lowest_value || min_key == nil
       lowest_value = b
       min_key = a
@@ -15,4 +15,4 @@ def key_for_min_value(name_hash)
   min_key
 end
 
-name_hash = {:adam =>
+#name_hash = {:adam =>
