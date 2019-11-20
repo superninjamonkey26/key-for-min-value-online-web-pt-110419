a2f9f36211ced38302c3c30573429aaf3b0ef4b6
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-20 19:16:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   min_key = 0
   lowest_value = 0
   name_hash.each do |a, b|
-    binding.pry
+   # binding.pry
     if lowest_value == 0 || b < lowest_value || min_key == 0
       lowest_value = b
       min_key = a
